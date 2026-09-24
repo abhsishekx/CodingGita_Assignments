4b478ca9750d4e1d1a16f7c86b3b8e2936827c5d
abhsishekx <abhi865121@gmail.com>
2026-09-24 11:56:14 +0530

    Create String Indexing exercises in new Python file
    Added string indexing exercises for practice.

---
+++ python_assignments/assigments03/String Indexing.py
@@ -0,0 +1,32 @@
+# # Topic 5 —  String Indexing
+
+# Q19
+# text = "PYTHON"
+# print(text[0])
+# print(text[1])
+# print(text[-1])
+# Output: `P`, `Y`, `N`, `O` (one per line).
+
+#Q20
+# text = "COMPUTER"
+# print(text[0])   # C
+# print(text[3])   # P
+# print(text[-1])  # R
+# print(text[-3])  # T
+
+
+#Q21
+
+# P
+# T
+# N
+# O
+
+
+# Q22
+# word = input()
+# print(word[0])
+# print(word[-1])
+
+#Q23
+#word[0] = "P", word[2] = "O", word[-1] = "M", word[-4] = "G"
