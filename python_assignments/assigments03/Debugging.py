31de862c92be80d2eb907d8cd36eb06589c4a928
abhsishekx <abhi865121@gmail.com>
2026-09-24 12:00:24 +0530

    Add debugging exercises in Debugging.py
    Added debugging exercises for practice.

---
+++ python_assignments/assigments03/Debugging.py
@@ -0,0 +1,26 @@
+# ## Topic 10 — Debugging
+
+#Q54 
+# age = int(input("Enter age: "))
+# print("Age after 5 years:", age + 5)
+
+
+#Q55
+
+# print("It's Python")
+
+
+#Q56 
+# text = "Python"
+# print(text[1:4])
+
+
+#Q57 
+# a, b = input().split()
+
+##Q58 
+# a, b = input().split()
+# print(int(a) + int(b))
+
+#Q59 
+# print("C:\\new\\test")
