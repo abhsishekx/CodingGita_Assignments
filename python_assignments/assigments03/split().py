9bf31a2bc2d85e5640a51d23b35017dcaf478f14
abhsishekx <abhi865121@gmail.com>
2026-09-24 11:58:52 +0530

    Create split.py with string split exercises
    Add split() exercises for string manipulation.

---
+++ python_assignments/assigments03/split().py
@@ -0,0 +1,52 @@
+# # Topic 7 — split()
+
+#Q33 Output: ['Python', 'is', 'easy']
+
+# Q34 Output: ['apple', 'banana', 'mango']
+
+#Q35 Output: ['Python is easy']
+
+#Q36
+# first, middle, last = input().split()
+# print(first)
+# print(middle)
+# print(last)
+
+
+#Q37
+
+# first_name, last_name = input().split()
+# print(f"First Name: {first_name}")
+# print(f"Last Name: {last_name}")
+
+
+#Q38
+
+# a, b, c = input().split()
+# total = int(a) + int(b) + int(c)
+# print(total)
+
+
+#Q39
+
+# name, age, course, city = input().split(",")
+# print(f"Name: {name}")
+# print(f"Age: {age}")
+# print(f"Course: {course}")
+# print(f"City: {city}")
+
+
+#Q40
+
+# username, domain = input().split("@")
+# print(f"Username: {username}")
+# print(f"Domain: {domain}")
+
+
+#Q41
+# sentence = input()
+# words = sentence.split()
+# print(f"First word: {words[0]}")
+# print(f"Last word: {words[-1]}")
+# print(f"Total words: {len(words)}")
+
