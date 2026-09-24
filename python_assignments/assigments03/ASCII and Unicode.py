20c602a5836a40ff7395ef28521f108ce89909fd
abhsishekx <abhi865121@gmail.com>
2026-09-24 11:53:50 +0530

    Create ASCII and Unicode demonstration script
    Added a Python script demonstrating ASCII and Unicode operations.

---
+++ python_assignments/assigments03/ASCII and Unicode.py
@@ -0,0 +1,32 @@
+ # Topic 4 — ASCII and Unicode
+
+#Q12
+# print(ord("A"), ord("a"), ord("Z"), ord("z"), ord("0"), ord("9"), ord("@"))
+
+# Q13 
+# print(chr(65), chr(66), chr(97), chr(98), chr(48), chr(57), chr(64))
+
+
+#Q14
+
+# ord("a") is larger
+# ord("b") - ord("B")
+
+# Q15
+# character = input()
+# print(ord(character))
+
+# Q16
+# letter = input()
+# print(chr(ord(letter) + 1))
+
+#Q17
+# True
+# True
+# True
+# True
+
+
+# Q18 
+# print(chr(9731), chr(9829), chr(8377))
+# print(ord("☃"), ord("♥"), ord("₹"))
