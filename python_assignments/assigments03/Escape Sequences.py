f1564e727b38a86a0a40790a29a60ad05272b156
abhsishekx <abhi865121@gmail.com>
2026-09-24 11:59:29 +0530

    Create Escape Sequences.py with example prints
    Added a Python script demonstrating various escape sequences.

---
+++ python_assignments/assigments03/Escape Sequences.py
@@ -0,0 +1,35 @@
+# # Topic 8 — Escape Sequences
+
+#Q42
+# print("Hello\nWorld")
+
+
+#Q43
+
+# print("Name:\tRahul")
+# print("Age:\t20")
+# print("City:\tAhmedabad")
+
+
+#Q44
+
+# print("C:\\Python\\Programs")
+
+
+#Q45
+# print('It\'s Python')
+
+
+#Q46
+# print("He said \"Hello\"")
+
+
+#Q47
+
+# Python
+# Programming
+
+
+#Q48
+
+# print("Student Details\n\nName:\tRahul\nAge:\t20\nCourse:\tB.Tech")
