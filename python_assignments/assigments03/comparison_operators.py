f3e8af21edf74a14da17dbcb17417c2f104eca82
abhsishekx <abhi865121@gmail.com>
2026-09-24 11:48:03 +0530

    Add comparison operators assignment file

---
+++ python_assignments/assigments03/comparison_operators.py
@@ -0,0 +1,30 @@
+#Topic 1 — Comparison Operators
+#Q1
+# text
+# True
+# False
+# False
+# True
+# True
+# False
+
+#Q2
+# True
+# False
+# False
+# True
+# True
+
+
+#Q3
+
+# True
+# True
+# False
+# True
+
+
+#Q4
+# True
+# False
+# True
